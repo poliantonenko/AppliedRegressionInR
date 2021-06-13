f73a79dc050324efca241b002959d0e1fe79f98b
Polina Antonenko <ulitkavvalenkax@gmail.com>
2021-06-13 16:45:54 +0200

    feat: add gitignore and r skript

---
+++ antonenko atestace.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+install.packages("jsonlite")
+install.packages("janitor")
+library(jsonlite)
+
+raw_bechdel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/raw_bechdel.csv')
+movies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv')
+
