e875eca117f2345670e2b23717cc5470100e56a2
Polina Antonenko <ulitkavvalenkax@gmail.com>
2021-06-15 22:11:46 +0200

    fix: remove packages installation

--- antonenko atestace.R
+++ antonenko atestace.R
@@ -1,7 +1,4 @@
 library(tidyverse)
-install.packages("jsonlite")
-install.packages("janitor")
-library(jsonlite)
 
 raw_bechdel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/raw_bechdel.csv')
 movies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv')
