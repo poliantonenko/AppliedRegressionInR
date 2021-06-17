48848824d8316a4747e5b1274b01510c5c9f934e
Polina Antonenko <ulitkavvalenkax@gmail.com>
2021-06-17 15:41:21 +0200

    feat: add recoding and initial charts

--- antonenko atestace.R
+++ antonenko atestace.R
@@ -3,3 +3,20 @@ library(tidyverse)
 raw_bechdel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/raw_bechdel.csv')
 movies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/movies.csv')
 
+
+raw_bechdel %>% ggplot(aes(x=year))+ geom_histogram()
+raw_bechdel %>%  ggplot(aes(x= rating))+ geom_histogram()
+
+
+movies <- movies %>% 
+  mutate(rated= as.factor(rated))
+
+
+movies<-movies %>%
+  mutate(rated = replace(rated, rated =="N/A", NA)) %>% 
+  mutate(rated = fct_collapse(rated, "NR"= c("Not Rated","Unrated"))) %>% 
+  mutate(rated= fct_collapse(rated, "NC-17"= c("NC-17", "X"))) %>% 
+  mutate(rated= fct_collapse(rated, "PG"= c("PG", "TV-PG"))) %>% 
+  mutate(rated= fct_collapse(rated, "PG-13"= c("PG-13", "TV-14")))
+
+movies %>%  ggplot(aes(x= rated))+ geom_bar()
