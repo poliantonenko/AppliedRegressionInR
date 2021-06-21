81776a9745544e52cd9faa15896eb55b7514ed43
Polina Antonenko <ulitkavvalenkax@gmail.com>
2021-06-21 13:47:07 +0200

    feat: replace NR with NA

--- antonenko atestace.R
+++ antonenko atestace.R
@@ -14,7 +14,8 @@ movies <- movies %>%
 
 movies<-movies %>%
   mutate(rated = replace(rated, rated =="N/A", NA)) %>% 
-  mutate(rated = fct_collapse(rated, "NR"= c("Not Rated","Unrated"))) %>% 
+  mutate(rated = fct_collapse(rated, "NR"= c("Not Rated","Unrated"))) %>%
+  mutate(rated = replace(rated, rated == "NR", NA)) %>% 
   mutate(rated= fct_collapse(rated, "NC-17"= c("NC-17", "X"))) %>% 
   mutate(rated= fct_collapse(rated, "PG"= c("PG", "TV-PG"))) %>% 
   mutate(rated= fct_collapse(rated, "PG-13"= c("PG-13", "TV-14")))
