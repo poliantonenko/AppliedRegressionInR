a73c97f8d537dcd7d2969423b4ac1082e0d1f5ec
Polina Antonenko <ulitkavvalenkax@gmail.com>
2021-06-25 14:19:32 +0200

    feat: add more predictors

--- antonenko atestace.R
+++ antonenko atestace.R
@@ -16,16 +16,29 @@ movies<-movies %>%
 
 movies %>%
   ggplot(aes(x= binary))+
-  geom_bar()
+  geom_bar()+
+  theme_minimal()
 
 movies %>%
   ggplot(aes(x= rated, fill= binary))+
-  geom_bar(position = "dodge")
+  geom_bar(position = "dodge")+
+  theme_minimal()
 
 movies %>%
   ggplot(aes(x=year, fill = binary))+
-  geom_bar(position = "stack")
+  geom_bar(position = "stack")+
+  theme_minimal()
 
 movies %>% 
   ggplot(aes(x= imdb_rating))+
-  geom_histogram()
+  geom_histogram()+
+  theme_minimal()
+
+movies<- movies %>%
+  mutate(binary= as.factor(binary))
+
+movies$binary = as.numeric(movies$binary)
+
+mod1 =lm(binary~ year+imdb_rating+metascore+rated+budget_2013, data=movies)
+is.numeric(movies$year)
+summary(mod1)
